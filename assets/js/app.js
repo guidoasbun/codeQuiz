23d8f40bf8eb2cf2973875d88540a87aee46751a
Guido <Asbun>
2020-01-28 22:51:47 -0800

    work on JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
+const start = document.getElementById("start")
+const questionContainer = document.getElementById("question-container")
+const next = document.getElementById("next-btn")
+const questionElement = document.getElementById("question")
+const answersButtonsElement = document.getElementById("answer-btns")
+const choiceA = document.getElementById("A")
+const choiceB = document.getElementById("B")
+const choiceC = document.getElementById("C")
+const choiceD = document.getElementById("D")
+
 let questions = [
   {
-    question: 'This is the first question',
+    question: 'This is the first question array',
     ansA: '1st answer',
     ansB: '2nd answer',
     ansC: '3rd answer',
@@ -73,14 +83,40 @@ let questions = [
   }
 ]
 
-const start = document.getElementById("start")
-const questionContainer = document.getElementById("question-container")
-const nextButton = document.getElementById("next-btn")
-const questionElement = document.getElementById("question")
-const answersButtonsElement = document.getElementById("answer-btns")
 
+let currentQuestionsIndex = 0
 
-start.addEventListener("click", function(){
+let score = 0
+
+function renderQuestion (){
+  let i = questions[currentQuestionsIndex]
+  questionElement.innerHTML = "<h3>" + i.question + "</h3>"
+  choiceA.innerHTML = i.ansA
+  choiceB.innerHTML = i.ansB
+  choiceC.innerHTML = i.ansC
+  choiceD.innerHTML = i.ansD
+
+}
+
+start.addEventListener("click", startQuiz)
+ 
+function startQuiz() {
   $("#intro-container").hide();
+  renderQuestion()
   $("#question-container").show();
+  nextQuestion()
+}
+
+next.addEventListener("click", function(){
+  currentQuestionsIndex ++
+  nextQuestion()
 })
+
+function nextQuestion(){
+  
+}
+
+
+
+
+  
