3dea25367daf860851291bcca8f6e0393663dc48
Guido <Asbun>
2020-01-29 19:48:24 -0800

    added countdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 const start = document.getElementById("start")
 const questionContainer = document.getElementById("question-container")
-// const next = document.getElementById("next-btn")
 const questionElement = document.getElementById("question")
 const answersButtonsElement = document.getElementById("answer-btns")
 const choiceA = document.getElementById("A")
@@ -8,6 +7,7 @@ const choiceB = document.getElementById("B")
 const choiceC = document.getElementById("C")
 const choiceD = document.getElementById("D")
 const score = document.getElementById("score")
+const timerElement = document.getElementById("timer-element")
 
 let questions = [
   {
@@ -27,34 +27,34 @@ let questions = [
     correct: "C"
   },
   {
-    question: "In HTML, the <title> element is located inside",
-    ansA: '<head> element',
-    ansB: '<body> element',
-    ansC: '<link> element',
+    question: 'In HTML, the &#60 title &#62 element is located inside',
+    ansA: '&#60 head &#62 element',
+    ansB: '&#60 body &#62 element',
+    ansC: '&#60 link &#62 element',
     ansD: 'Is not an element of HTML',
     correct: "B"
   },
   {
-    question: 'In HTML, Where can we use the <style> tag?',
-    ansA: 'Just in <head> element',
-    ansB: 'Just in <body> element',
-    ansC: 'In <head> and in <body> elements',
-    ansD: '<style> is not an HTML element',
+    question: 'In HTML, Where can we use the &#60 style> tag?',
+    ansA: 'Just in &#60 head &#62 element',
+    ansB: 'Just in &#60 body &#62 element',
+    ansC: 'In &#60 head &#62 and in &#60 body &#62 elements',
+    ansD: '&#60 tyle &#62 is not an HTML element',
     correct: "A"
   },
   {
     question: 'Inside which HTML element do we put the JavaScript?',
-    ansA: '<js>',
-    ansB: '<javascript>',
-    ansC: '<script>',
-    ansD: '<scripting>',
+    ansA: '&#60 js &#62',
+    ansB: '&#60 javascript &#62',
+    ansC: '&#60 script &#62',
+    ansD: '&#60 scripting &#62',
     correct: "C"
   },
   {
     question: 'Where is the correct place to insert JavaScript?',
-    ansA: 'The <head> section',
-    ansB: 'Both the <head> and the <body> section',
-    ansC: 'The <body> section',
+    ansA: 'The &#60 head &#62 section',
+    ansB: 'Both the &#60 head &#62 and the &#60 body &#62 section',
+    ansC: 'The &#60 body &#62 section',
     ansD: 'You do not isert JavaScript',
     correct: "B"
   },
@@ -68,18 +68,18 @@ let questions = [
   },
   {
     question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
-    ansA: 'if(i<>5)',
+    ansA: 'if(i&#60  &#62 5)',
     ansB: 'if i=! then',
-    ansC: 'if i <> 5',
+    ansC: 'if i &#60  &#62 5',
     ansD: 'if(i !=5)',
     correct: "D"
   },
   {
     question: 'Where in an HTML document is the correct place to refer to an external style sheet?',
-    ansA: 'In the <body> section',
-    ansB: 'In the <head> section',
+    ansA: 'In the &#60 body &#62 section',
+    ansB: 'In the &#60 head &#62 section',
     ansC: 'At the end of the document',
-    ansD: 'Outside the <html>',
+    ansD: 'Outside the &#60 html &#62',
     correct: "B"
   },
   {
@@ -95,6 +95,8 @@ let questions = [
 let lastQuestionIndex = questions.length -1 
 let currentQuestionsIndex = 0
 let totalScore = 0
+let quizTime = 5 + 1
+
 
 function renderQuestion (){
   let i = questions[currentQuestionsIndex]
@@ -111,6 +113,8 @@ function startQuiz() {
   $("#intro-container").hide()
   renderQuestion()
   $("#question-container").show()
+  renderTimer()
+  let quizTimer = setInterval(renderTimer, 1000)
 }
 
 function checkAnswer(answer) {
@@ -131,6 +135,19 @@ function renderScore (){
   let percent = (totalScore / questions.length) * 100
   score.innerHTML = "<h3>Your Score is: " + percent + "%</h3>"
 }
+
+function renderTimer(){
+  quizTime --
+  timerElement.innerHTML = 'Time remaining: '+ quizTime
+  if (quizTime === 0){
+    $("#question-container").hide()
+    $("#score-container").show()
+    renderScore()
+  }
+  
+}
+
+
 
 
 
